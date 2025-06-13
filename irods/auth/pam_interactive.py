bca661e8cdb3c3c25600e94b15fe03f541b3bd46
khansameer25 <realskhan25@gmail.com>
2025-06-13 19:46:14 -0400

    implement steps to authenticate user and store password

--- irods/auth/pam_interactive.py
+++ irods/auth/pam_interactive.py
@@ -4,7 +4,13 @@ from . import (
     authentication_base,
     _auth_api_request,
     FORCE_PASSWORD_PROMPT,
+    throw_if_request_message_is_missing_key,
+    AuthStorage,
+    STORE_PASSWORD_IN_MEMORY,
+    CLIENT_GET_REQUEST_RESULT
 )
+
+import getpass
 
 AUTH_CLIENT_AUTH_REQUEST = "pam_auth_client_request"
 AUTH_CLIENT_AUTH_RESPONSE = "pam_auth_response"
@@ -25,16 +31,22 @@ AUTH_AGENT_AUTH_REQUEST = "auth_agent_auth_request"
 AUTH_AGENT_AUTH_RESPONSE = "auth_agent_auth_response"
 
 def login(conn, **extra_opt):
-    auth_client_object = _pam_interactive_ClientAuthState(conn, scheme=PAM_INTERACTIVE_SCHEME)
+    depot = AuthStorage.create_temp_pw_storage(conn)
+
+    auth_client_object = _pam_interactive_ClientAuthState(conn, depot, scheme=PAM_INTERACTIVE_SCHEME)
     auth_client_object.authenticate_client(
         initial_request=extra_opt
     )
 
 class _pam_interactive_ClientAuthState(authentication_base):
-    def __init__(self, *_, **_kw):
-        super().__init__(*_, **_kw)
+    def __init__(self, conn, depot, *_, **_kw):
+        super().__init__(conn, *_, **_kw)
+        self.depot = depot
+        self._list_for_request_result_return = None
 
     def auth_client_start(self, request):
+        self._list_for_request_result_return = request.pop(CLIENT_GET_REQUEST_RESULT, None)
+
         resp = request.copy()
 
         resp["pstate"] = resp.get("pstate", {})
@@ -57,5 +69,130 @@ class _pam_interactive_ClientAuthState(authentication_base):
 
         resp = _auth_api_request(self.conn, server_req)
         resp[__NEXT_OPERATION__] = AUTH_CLIENT_AUTH_RESPONSE
+
+        return resp
+
+    def pam_auth_response(self, request):
+        throw_if_request_message_is_missing_key(request, ["user_name", "zone_name"])
+
+        server_req = request.copy()
+        server_req["next_operation"] = AUTH_AGENT_AUTH_RESPONSE
+
+        resp = _auth_api_request(self.conn, server_req)
+
+        return resp
+
+    def _get_default_value(self, request):
+        default_path = request.get("msg", {}).get("default_path", "")
+        default_value = ""
+
+        if default_path and default_path.startswith('/'):
+            key = default_path[1:]
+            pstate = request.get("pstate", {})
+
+            if key in pstate:
+                default_value = pstate[key]
+
+        return default_value
+
+    def _patch_state(self, req):
+        patch_ops = req.get("msg", {}).get("patch")
+        if not patch_ops:
+            return
+
+        pstate = req.get("pstate", {})
+        resp = req.get("resp", "")
+
+        for op in patch_ops:
+            path = op.get("path", "")
+            if not path.startswith('/'):
+                continue
+
+            key = path[1:]
+            operation = op.get("op")
+
+            value = op.get("value") if "value" in op else resp
+
+            if operation == "add" or operation == "replace":
+                pstate[key] = value
+            elif operation == "remove":
+                pstate.pop(key, None)
+
+        req["pstate"] = pstate
+        req["pdirty"] = True
+        del req["msg"]["patch"]
+
+    def _retrieve_entry(self, req):
+        if "retrieve" not in req.get("msg", {}):
+            return False
+
+        retr_path = req["msg"].get("retrieve", "")
+        if retr_path and retr_path.startswith('/'):
+            key = retr_path[1:]
+            pstate = req.get("pstate", {})
+            if key in pstate:
+                req["resp"] = pstate[key]
+                return True
+
+        req["resp"] = ""
+        return True
+
+    def waiting_pw(self, request):
+        server_req = request.copy()
+
+        if self._retrieve_entry(server_req):
+            self._patch_state(server_req)
+            server_req["next_operation"] = AUTH_AGENT_AUTH_RESPONSE
+            return _auth_api_request(self.conn, server_req)
+
+        prompt = server_req.get("msg", {}).get("prompt", "Password: ")
+        default_value = self._get_default_value(server_req)
+
+        display_prompt = prompt
+        if default_value:
+            display_prompt = f"{prompt} [******] "
+
+        pw = getpass.getpass(display_prompt)
+
+        if not pw and default_value:
+            server_req["resp"] = default_value
+        else:
+            server_req["resp"] = pw
+
+        self._patch_state(server_req)
+        server_req["next_operation"] = AUTH_AGENT_AUTH_RESPONSE
+
+        return _auth_api_request(self.conn, server_req)
+
+    def next(self, request):
+        prompt = request.get("msg", {}).get("prompt", "")
+        if prompt:
+            print(prompt)
+
+        server_req = request.copy()
+        self._patch_state(server_req)
+        server_req[__NEXT_OPERATION__] = AUTH_AGENT_AUTH_RESPONSE
+
+        resp = _auth_api_request(self.conn, server_req)
+
+        return resp
+
+    def authenticated(self, request):
+        throw_if_request_message_is_missing_key(request, ["request_result"])
+        pw = request["request_result"]
+
+        if not self.depot:
+            raise RuntimeError("auth storage object was either not set, or allowed to expire prematurely.")
+
+        if request.get(STORE_PASSWORD_IN_MEMORY):
+            self.depot.use_client_auth_file(None)
+
+        self.depot.store_pw(pw)
+
+        if isinstance(self._list_for_request_result_return, list):
+            self._list_for_request_result_return[:] = (pw,)
+
+        resp = request.copy()
+        resp[__NEXT_OPERATION__] = PERFORM_NATIVE_AUTH
 
         return resp
