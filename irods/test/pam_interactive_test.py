dff7763d15008bf1f6bfe5ac865e44f4535311da
khansameer25 <realskhan25@gmail.com>
2025-07-31 10:27:31 -0400

    add tests for basic auth and .irodsA creation

--- irods/test/pam_interactive_test.py
+++ irods/test/pam_interactive_test.py
@@ -30,5 +30,45 @@ class PamInteractiveTest(unittest.TestCase):
         if self.sess:
             self.sess.cleanup()
 
+    def test_pam_interactive_login_basic(self):
+        with patch("getpass.getpass", return_value=self.password):
+            self.sess = make_session(test_server_version=False, env_file=self.env_file_path, authentication_scheme="pam_interactive")
+            self.assertTrue(self.sess.server_version)
+            self.assertEqual(self.sess.username, self.user)
+            self.assertEqual(self.sess.zone, self.zone)
+
+    def test_pam_interactive_auth_file_creation(self):
+        with patch("getpass.getpass", return_value=self.password):
+            write_pam_interactive_irodsA_file(env_file=self.env_file_path)
+            self.assertTrue(os.path.exists(self.auth_file_path), ".irodsA file was not created")
+
+            self.sess = make_session(test_server_version=False, env_file=self.env_file_path, authentication_scheme="pam_interactive")
+            self.assertTrue(self.sess.server_version)
+            self.assertEqual(self.sess.username, self.user)
+            self.assertEqual(self.sess.zone, self.zone)
+
+    def test_forced_interactive_flow(self):
+        with patch("getpass.getpass", return_value=self.password):
+            write_pam_interactive_irodsA_file(env_file=self.env_file_path)
+
+        self.assertTrue(os.path.exists(self.auth_file_path), ".irodsA file was not created for forced flow test")
+
+        with patch("getpass.getpass", return_value=self.password) as mock_getpass:
+            self.sess = make_session(test_server_version=False, env_file=self.env_file_path, authentication_scheme="pam_interactive", force_password_prompt=True)
+            self.assertTrue(self.sess.server_version)
+            self.assertEqual(self.sess.username, self.user)
+            self.assertEqual(self.sess.zone, self.zone)
+            self.assertTrue(mock_getpass.called, "Password prompt was not forced when FORCE_PASSWORD_PROMPT=True")
+
+    def test_failed_login_incorrect_password(self):
+        with patch("getpass.getpass", return_value="wrong_password"):
+            with self.assertRaises(ClientAuthError):
+                sess = make_session(test_server_version=False, env_file=self.env_file_path, authentication_scheme="pam_interactive", force_password_prompt=True)
+                server_version = sess.server_version # trigger auth flow
+
+        with patch("getpass.getpass", return_value="wrong_password"):
+            with self.assertRaises(ClientAuthError):
+                write_pam_interactive_irodsA_file(env_file=self.env_file_path)
+
 if __name__ == "__main__":
     unittest.main()
